1f69c9998ec3dc43bee18953553e9c1c7517f920
Vaibhav Vishwakarma <145886058+spygamingog@users.noreply.github.com>
2026-02-06 04:05:17 +0530

    Delete src/main directory

--- src/main/java/com/spygamingog/spycore/api/DataService.java
+++
@@ -1,8 +0,0 @@
-package com.spygamingog.spycore.api;
-
-public interface DataService {
-    void save(String key, Object value);
-    Object load(String key);
-    <T> T load(String key, Class<T> type);
-    void delete(String key);
-}
